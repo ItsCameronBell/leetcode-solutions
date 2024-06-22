c84b1c650f9a783a0a2bc4f6baf7960175c79ff1
ItsCameronBell <cameron352bell@gmail.com>
2024-06-22 17:57:28 +0100

    feat: Created and solved 1342-number-of-steps-to-reduce-a-number-to-zero.py

---
+++ python3/1342-number-of-steps-to-reduce-a-number-to-zero.py
@@ -0,0 +1,16 @@
+from typing import List
+
+"""
+Time complexity: O(Log(N))
+Space complextiy: O(1)
+"""
+class Solution:
+    def numberOfSteps(self, num: int) -> int:
+        q = 0
+        while num != 0:
+            if num % 2 == 0:
+                num /= 2
+            else:
+                num -= 1
+            q += 1
+        return q
