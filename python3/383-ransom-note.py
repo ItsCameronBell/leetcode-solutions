8b517165e6dd28f5d5158b65f3ae48858c5785c5
ItsCameronBell <cameron352bell@gmail.com>
2024-06-22 20:23:47 +0100

    feat: Created and solve 383-ransom-note.py

---
+++ python3/383-ransom-note.py
@@ -0,0 +1,11 @@
+"""
+Time complexity: 
+Space complextiy: 
+"""
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        for r in ransomNote:
+            if r in magazine:
+                magazine = magazine.replace(r, "", 1) 
+            else: return False
+        return True
