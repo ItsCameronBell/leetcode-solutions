782d39841c7ec77416751a49781389e6eeb36e97
ItsCameronBell <cameron352bell@gmail.com>
2024-06-22 17:49:53 +0100

    feat: Created and solved 412-fizz-buzz.py

---
+++ python3/412-fizz-buzz.py
@@ -0,0 +1,15 @@
+from typing import List
+
+"""
+Time complexity: O(N)
+Space complextiy: O(1)
+"""
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        q = []
+        for i in range(1, n + 1):
+            s = ""
+            if i % 3 == 0: s += "Fizz"
+            if i % 5 == 0: s += "Buzz"
+            q.append(str(i) if not s else s)
+        return q
